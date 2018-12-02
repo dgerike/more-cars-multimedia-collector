b8c66e37d7e34e1bbbebd0c21c58f7cba7d20f7a
Dennis Gerike <dennis.gerike@gmail.com>
2018-12-02 12:14:58 +0100

    add support for flickr and wikimedia

--- background.js
+++ background.js
@@ -1,9 +1,33 @@
 'use strict';
 
+var availableServices = [
+    {
+        "name": "flickr",
+        "selector": "flickr.com",
+        "url": "https://more-cars.net/image/create?image_platform=flickr&image_id=",
+    }, {
+        "name": "wikimedia",
+        "selector": "wikimedia.org/wiki/File:",
+        "url": "https://more-cars.net/image/create?image_platform=wikimedia&image_id=",
+    }, {
+        "name": "wikimedia",
+        "selector": "wikipedia.org/wiki/File:",
+        "url": "https://more-cars.net/image/create?image_platform=wikimedia&image_id=",
+    }, {
+        "name": "youtube",
+        "selector": "youtube.com/watch?v=",
+        "url": "https://more-cars.net/video/create?video_platform=youtube&video_id=",
+    }
+];
+
 chrome.browserAction.onClicked.addListener(function () {
     chrome.tabs.getSelected(function (tab) {
         var currenUrl = tab.url;
-        var targetUrl = "https://more-cars.net/video/create?video_platform=youtube&video_id=" + currenUrl;
-        chrome.tabs.create({url: targetUrl});
+        availableServices.forEach(function (service) {
+            if (currenUrl.includes(service.selector)) {
+                var targetUrl = service.url + tab.url;
+                chrome.tabs.create({url: targetUrl});
+            }
+        })
     });
 });
